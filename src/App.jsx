d4be5e0f22df073e31609f2c0cf50135ea3130e7
Akshatcodegenics <akshatr056@gmail.com>
2025-10-28 16:49:12 +0530

    2 commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
 
 import React from "react";
 import Routes from "./Routes";
-import Watermark from "./components/Watermark";
-import './styles/watermark.css';
+
 
 function App() {
   return (
-    <>
-      <Watermark />
+    
       <Routes />
-    </>
+  
   );
 }
 
